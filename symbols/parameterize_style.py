e833f0463c8224a42907affcb5b1c51e2e572f37
Max <none@none>
2015-01-01 12:12:24 +1100

    Updated for 2.6, Added symbols and converted soil samples from gpx to shapefiles

---
+++ symbols/parameterize_style.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+import sys, codecs, argparse
+from lxml import etree
+ 
+NS = { 'svg':"http://www.w3.org/2000/svg" }
+ 
+parser = argparse.ArgumentParser(description="My first Inkscape effect")
+parser.add_argument('--id', action="append", help="id(s) of selected elements")
+parser.add_argument('path', help="path of svg file to open")
+ 
+args = parser.parse_args()
+ 
+# Open the file & parse to a "etree" object
+f = codecs.open(args.path, encoding="utf-8")
+t = etree.parse(f)
+ 
+# I. Select and alter existing elements
+#
+# Loop over all svg:path elements
+for path in t.xpath("//svg:path ", namespaces=NS):
+    style = path.get("style")
+    if style:
+        try:
+            del path.attrib["style"]
+        except:
+            pass
+        path.set("fill", "param(fill) none")
+        path.set("stroke", "param(outline) #00ff00")
+        path.set("stroke-width", "param(outline-width) 1")
+# Loop over all svg:g elements
+for path in t.xpath("//svg:g ", namespaces=NS):
+    style = path.get("style")
+    if style:
+        try:
+            del path.attrib["style"]
+        except:
+            pass
+        path.set("fill", "param(fill) none")
+        path.set("stroke", "param(outline) #00ff00")
+        path.set("stroke-width", "param(outline-width) 1")
+# Loop over all svg:polyline elements
+for path in t.xpath("//svg:polyline ", namespaces=NS):
+    style = path.get("style")
+    if style:
+        #try:
+        #    del path.attrib["style"]
+        #except:
+        #    pass
+        path.set("fill", "param(fill) none")
+        path.set("stroke", "param(outline) #00ff00")
+        path.set("stroke-width", "param(outline-width) 1")
+
+# Output the (modified) tree
+sys.stdout.write(etree.tostring(t, encoding="utf-8", xml_declaration=True))
