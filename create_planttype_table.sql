a83a85866d0473277049b5a8c397e3a71a24e896
Max <max.d.bott@gmail.com>
2016-04-16 23:00:38 +1000

    Updated Farm project to 2.14

---
+++ create_planttype_table.sql
@@ -0,0 +1,23 @@
+﻿-- Table: botanical_name
+
+-- DROP TABLE planttype;
+CREATE SEQUENCE planttype_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE planttype_id_seq
+  OWNER TO postgres;
+CREATE TABLE planttype
+(
+  id smallint NOT NULL DEFAULT nextval('planttype_id_seq'::regclass),
+  type character varying(100) DEFAULT NULL::character varying,
+  CONSTRAINT planttype_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE planttype
+  OWNER TO postgres;
+
---
+++ vegetation_types.sql
@@ -0,0 +1,39 @@
+﻿-- View: vegetation_types
+--
+-- DROP VIEW vegetation_types;
+
+CREATE OR REPLACE VIEW vegetation_types AS 
+SELECT 
+  vegetation_types.row_number,
+  vegetation_types.botanicalid,
+  vegetation_types.family,
+  vegetation_types.genus,
+  vegetation_types.species,
+  vegetation_types.common_name,
+  vegetation_types.typeid,
+  vegetation_types.type
+FROM  ( SELECT row_number() OVER (ORDER BY botanical_name.common_name) AS row_number,
+            botanical_name.id AS botanicalid,
+            botanical_name.legacy_pfaf_latin_name,
+            botanical_name.family,
+            botanical_name.genus,
+            botanical_name.species,
+            botanical_name.common_name,
+            botanical_name.type AS typeid,
+            planttype.type
+           FROM botanical_name
+             LEFT JOIN planttype ON botanical_name.type = planttype.id
+           WHERE botanical_name.id IN
+            (SELECT DISTINCT botanical_name_id FROM vegetation)) vegetation_types;
+ALTER TABLE vegetation_types
+  OWNER TO postgres;
+COMMENT ON VIEW vegetation_types
+  IS '-- View: vegetation_types';
+
+  -- Rule: view_update ON vegetation_view
+
+-- DROP RULE view_update ON vegetation_view;
+
+CREATE OR REPLACE RULE view_type_update AS
+    ON UPDATE TO vegetation_types DO INSTEAD  UPDATE botanical_name SET type = new.typeid
+  WHERE botanical_name.id = new.botanicalid;
